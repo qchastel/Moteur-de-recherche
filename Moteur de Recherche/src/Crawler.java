755152b67afcd6d5948104af4103c52eeb5afc19
ecoatelant <ecoatelant@iut.univ-paris8.fr>
2020-01-28 18:01:35 +0100

    Methode parcoursFichiers

--- Moteur de Recherche/src/Crawler.java
+++ Moteur de Recherche/src/Crawler.java
@@ -5,7 +5,7 @@ import java.util.Set;
 public class Crawler {
 	
 	public static void main(String[] args) {
-		parcoursFichiers("/home/etudiants/info/ecoatelant/S4/RechercheDoc/corpusRI/D0821D");
+		parcoursFichiers("/home/etudiants/info/ecoatelant/S4/RechercheDoc/corpusRI"); //Pour Emilie : "/home/etudiants/info/ecoatelant/S4/RechercheDoc/corpusRI"
 	}
 	
 	Index index = new Index();
@@ -16,22 +16,18 @@ public class Crawler {
 		
 		Set<String> listeChemins = new HashSet<>();
 		
-	    File repertoire = new File(cheminMoteurRecherche);	//Pour Emilie : "/home/etudiants/info/ecoatelant/S4/RechercheDoc/"
+	    File repertoire = new File(cheminMoteurRecherche);
 	    String liste[] = repertoire.list();
 	    if (liste != null) {         
 	    	for (int i = 0; i < liste.length; i++) {
 	    		File fichierCourant = new File(liste[i]);
-	    		System.out.println(fichierCourant.toString());
-	    		System.out.println(liste[i].isFile());
-	    		if(!liste[i].is) {
-	    			parcoursFichiers(repertoire+fichierCourant.toString());
+	    		System.out.println(repertoire+"/"+fichierCourant);
+	    		if(!fichierCourant.isFile()) {
+	    			parcoursFichiers(repertoire+"/"+fichierCourant);
 	    		} else if (fichierCourant.isFile()){
-	    			listeChemins.add(fichierCourant.list()[0]);	
-	    			System.out.println(fichierCourant);
+	    			listeChemins.add(fichierCourant.list()[0]);
 	    		}
 	        }
-	    } else {
-	   	//System.err.println("Nom de repertoire invalide");
 	    }
 	}
 
