97bc36bd6b1ac2d681ea8bdcaa8e9a563416cf7d
qchastel <qchastel@iut.univ-paris8.fr>
2020-01-28 15:46:19 +0100

    commit 1

---
+++ Moteur de Recherche/src/Crawler.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Crawler{
+	
+	public Index i=new Index();;
+	public IndexInversé ii=new IndexInversé();;
+	public TraitementTexte tt=new TraitementTexte();
+
+	//Parcours l'ensemble de fichier, fait une liste de mot pour chaque document et remplis si jamais le terme s'il n'est pas présent.
+	public void remplissageIndex() {
+		//TO-DO
+	}
+	
+	public String RecupTexte(String Chemin) {
+		
+		return "";
+	}
+	
+	// rempli un index 
+	public void rempliIndex() {
+		
+		
+	} 
+	
+}
