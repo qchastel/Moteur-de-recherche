95477afb3df03e3cfef2c34f476bafd9e2e987d7
ecoatelant <ecoatelant@iut.univ-paris8.fr>
2020-02-24 14:58:39 +0100

    Commit du 24-02-2020 14:57

--- Moteur de Recherche/src/IndexInversé.java
+++ Moteur de Recherche/src/IndexInversé.java
@@ -5,30 +5,50 @@ import java.util.TreeSet;
 
 public class IndexInversé {
 	
-	public HashMap<String,TreeSet<String>> indexInverse;
+	//Exemple de format : Mot -> liste de documents où le mot apparaît
+	public HashMap<String,TreeSet<HashMap<String,Integer>>> registreMots;
 	
-	public IndexInversé() {
-		
-		indexInverse = new HashMap<String,TreeSet<String>>() ;
+	public IndexInversé(){
+		registreMots = new HashMap<String,TreeSet<HashMap<String,Integer>>>() ;
 	}
 	
-	public void indexé(ArrayList<String> texte, String nomDoc){
-		
-		for(String s :texte) {
-			if(!indexInverse.containsKey(s)) {
-				TreeSet<String> a = new TreeSet<>();
-				a.add(nomDoc);
-				indexInverse.put(s,a);
+	public void indexation(ArrayList<String> motDuTexte, String nomDoc){
+		//Pour chaque mot d'un texte donné, on regarde si ce mot est déjà indexé.
+		for(String mot : motDuTexte) {
+			//Si non alors on créé un nouveau mot dans registreMots
+			//avec un nouveau TreeSet qui contiendra tout les textes où apparaîtront ce même mot et sa fréquence initialisé à 1.
+			if(!registreMots.containsKey(mot)) {
+				TreeSet<HashMap<String,Integer>> listeDoc = new TreeSet<>();
+				HashMap<String,Integer> motEtFrequence = new HashMap<>();
+				motEtFrequence.put(nomDoc, 1);
+				listeDoc.add(motEtFrequence);
+				registreMots.put(mot,listeDoc);
 			}
+			//Si oui alors on trouve le mot et on regarde si dans la liste de documents, le document actuel est présent.
+			//Si oui alors on ajoute 1 à la fréquence, si non alors il raoute le document à la liste et initialise sa fréquence à 1.
 			else {
-				TreeSet<String> a = indexInverse.get(s);
-				a.add(nomDoc);
-				indexInverse.replace(s,a);
+				TreeSet<HashMap<String,Integer>> listeDocsMotCourant = registreMots.get(mot);
+				for(HashMap<String,Integer> mF : listeDocsMotCourant) { //TODO : remplacer par WHILE
+					if(mF.containsKey(nomDoc)) {
+						/*HashMap<String,Integer> pivot = mF;
+						pivot.replace(nomDoc, mF.get(nomDoc)+1);
+						listeDocsMotCourant.remove(mF);
+						listeDocsMotCourant.add(pivot);*/
+						mF.replace(nomDoc, mF.get(nomDoc)+1);
+						registreMots.replace(mot,listeDocsMotCourant);
+					}else {
+						HashMap<String,Integer> mFNVMot = new HashMap<> ();
+						mFNVMot.put(nomDoc, 1);
+						listeDocsMotCourant.add(mFNVMot);
+						registreMots.replace(mot,listeDocsMotCourant);
+					}
+				}
 			}
 		}
+		
 	}
-	 public HashMap<String, TreeSet<String>> getIndexInverse() {
-		return indexInverse;
+	 public HashMap<String,TreeSet<HashMap<String,Integer>>> getIndexInverse() {
+		return registreMots;
 	}
 	
 
