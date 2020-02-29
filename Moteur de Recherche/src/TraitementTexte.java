af33f64e7120a6dd9fa5dd9cdac267fd7bd209e6
qchastel <49200797+qchastel@users.noreply.github.com>
2020-02-29 16:12:13 +0100

    Traitemnt txt

--- Moteur de Recherche/src/TraitementTexte.java
+++ Moteur de Recherche/src/TraitementTexte.java
@@ -2,39 +2,55 @@ import java.util.ArrayList;
 
 public class TraitementTexte {
 	
-	public ArrayList<String> traiterTexte(String texte) {
-		return  Lemmatisation(NonMotsVide(Normalise(RecupTermes(texte))));
+	private ArrayList<String> motvide;
+	
+	public TraitementTexte() {
+	
+	motvide=new ArrayList<>();
+	motvide.add("Je");
+	motvide.add("un");
+	motvide.add("qui");
+	motvide.add("de");
+	motvide.add("ou");
+	motvide.add("pas");
 	}
 	
 	
 	public ArrayList<String> RecupTermes(String texte){
+		ArrayList<String> termes =new ArrayList<>();
+		String[]t=texte.split(" ");
+		for (String string : t) {
+			termes.add(string.trim());
+		}
+		return termes;
 		
-		
-		return null;
 	} 
 	
 	public ArrayList<String> Normalise(ArrayList<String> termes){
-		
-		NonMotsVide(termes);
-		Lemmatisation(termes);
-		
-		return termes;
-	}
-	
-	public ArrayList<String> NonMotsVide(ArrayList<String> termes){
-		
-		
-		
-		return termes;
-	}
-	
-	public ArrayList<String> Lemmatisation (ArrayList<String> termes){
-		
-		return termes;
-	}
 
-	public static ArrayList<String> traitéTexte(String s){
-		return null;
+		Lemmatisation(termes);
+		NonMotsVide(termes);
 		
+		return termes;
+	}
+	
+	private ArrayList<String> NonMotsVide(ArrayList<String> termes){
+		
+		for (String string : motvide) {
+			for (int i=0;i<termes.size();i++) {
+				if(string.equals(termes.get(i))) {
+					termes.remove(termes.get(i));
+					i=termes.size();
+				}
+			}
+		}
+		
+		return termes;
+	}
+	
+	private ArrayList<String> Lemmatisation (ArrayList<String> termes){
+		
+		
+		return termes;
 	}
 }
